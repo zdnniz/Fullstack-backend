1d5e00235cef694c551ba7768fd0354da5856f98
zdnniz <zdennis076@gmail.com>
2025-06-20 14:47:18 -0400

    backend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config({ path: "./config/config.env" });
 
 app.use(
     cors({
-        origin: 'https://fullstack-frontend-yanboc.netlify.app',
+        origin: 'http://localhost:5173',
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     })
