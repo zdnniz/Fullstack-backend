0f76f0ef0f1dfb6015025886093c23aec6fc9a82
zdnniz <zdennis076@gmail.com>
2025-06-22 14:18:33 -0400

    backend

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax'
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     }
 }));
 
